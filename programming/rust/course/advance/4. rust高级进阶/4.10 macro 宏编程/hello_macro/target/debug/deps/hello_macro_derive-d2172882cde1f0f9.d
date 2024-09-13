5bd58967800c668a483c11e359cd5b7308a1da98
Benxiaohai001 <1447333108@qq.com>
2024-09-13 17:55:21 +0800

    20240913

---
+++ programming/rust/course/advance/4. rust高级进阶/4.10 macro 宏编程/hello_macro/target/debug/deps/hello_macro_derive-d2172882cde1f0f9.d
@@ -0,0 +1,7 @@
+/Users/bichaoxue/Documents/github.com/collection/programming/rust/course/advance/4. rust高级进阶/4.10 macro 宏编程/hello_macro/target/debug/deps/libhello_macro_derive-d2172882cde1f0f9.rmeta: hello_macro_derive/src/lib.rs
+
+/Users/bichaoxue/Documents/github.com/collection/programming/rust/course/advance/4. rust高级进阶/4.10 macro 宏编程/hello_macro/target/debug/deps/libhello_macro_derive-d2172882cde1f0f9.rlib: hello_macro_derive/src/lib.rs
+
+/Users/bichaoxue/Documents/github.com/collection/programming/rust/course/advance/4. rust高级进阶/4.10 macro 宏编程/hello_macro/target/debug/deps/hello_macro_derive-d2172882cde1f0f9.d: hello_macro_derive/src/lib.rs
+
+hello_macro_derive/src/lib.rs:
