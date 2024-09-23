39d1f66fa268b4a4b3edac382082b51b038d51df
Benxiaohai001 <1447333108@qq.com>
2024-09-23 10:03:47 +0800

    20240923

--- programming/rust/test001/src/main.rs
+++ programming/rust/test001/src/main.rs
@@ -9,12 +9,17 @@ fn main() {
     // unwrap() 方法
     // let un: Result<u32, String> = Ok(32);
     // assert_eq!(un.unwrap(), 32);
-    let un2: Result<u32, String> = Err("error".to_string());
-    assert_eq!(un2.unwrap(), 32);
-    for i in 0..10 {
-        do_something();
-        // println!("{}", i);
-    }
+    // let un2: Result<u32, String> = Err("error".to_string());
+    // assert_eq!(un2.unwrap(), 32);
+    // for i in 0..10 {
+    //     do_something();
+    //     // println!("{}", i);
+    // }
+    // let x = 3.1415;
+    let x = std::f64::consts::PI;
+    let r = 8.0;
+    println!("the area of the circle is {}", x * r * r);
+    println!("{}", x);
 }
 
 fn do_something() {}
---
+++ programming/rust/test001/target/debug/deps/test001-828a0d6c866e7bd6.d
@@ -0,0 +1,9 @@
+/Users/bichaoxue/Documents/github.com/collection/programming/rust/test001/target/debug/deps/libtest001-828a0d6c866e7bd6.rmeta: src/main.rs Cargo.toml
+
+/Users/bichaoxue/Documents/github.com/collection/programming/rust/test001/target/debug/deps/test001-828a0d6c866e7bd6.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
