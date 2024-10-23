df79a42b3497d360621a73154f904be62657636f
Benxiaohai001 <1447333108@qq.com>
2024-10-23 11:45:41 +0800

    20241023

--- programming/cpp/tutorial/5.Operators/operators.cpp
+++ programming/cpp/tutorial/5.Operators/operators.cpp
@@ -18,12 +18,37 @@ int main()
     // a += 2;
     // cout << "a:" << a << endl;
     // 自增/自减运算符
-    int a, b = 10;
-    a = b++;
-    cout << "a:" << a << endl;
-    cout << "b:" << b << endl;
-    int c, d = 10;
-    c = ++d;
-    cout << "c:" << c << endl;
-    cout << "d:" << d << endl;
+    // int a, b = 10;
+    // a = b++;
+    // cout << "a:" << a << endl;
+    // cout << "b:" << b << endl;
+    // int c, d = 10;
+    // c = ++d;
+    // cout << "c:" << c << endl;
+    // cout << "d:" << d << endl;
+
+    // 关系元素安抚和比较运算符
+    cout << (5 > 3) << endl;
+    cout << (5 < 3) << endl;
+    cout << (5 >= 3) << endl;
+    cout << (5 <= 3) << endl;
+    cout << (5 == 3) << endl;
+    cout << (5 != 3) << endl;
+
+    int a = 5;
+    int b = 3;
+    int c = 7;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a > b && a < c) << endl;
+    cout << (a == 5) << endl;
+    cout << (a == 3) << endl;
+    cout << (a*b > c)  << endl;
+    cout << (a*b < c)  << endl;
+    cout << ((b = 2) < 10) << endl;
+    return 0;
 }
