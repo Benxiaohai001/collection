dab03e83c402f94d26c05112e08fdaea8e468420
bca <1447333108@qq.com>
2025-01-20 05:34:42 +0800

    20250120

--- programming/python/doc/5 数据结构/树/树.py
+++ programming/python/doc/5 数据结构/树/树.py
@@ -179,3 +179,63 @@ def printexp(tree):
         sVal += printexp(tree.getRightChild()) + ')'
     return sVal 
 
+# 验证二叉搜索树
+# 力扣 98
+# 递归求解
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, lower = float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+        return helper(root)
+# 中序遍历求解
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack, inorder = [], float('-inf')
+
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+        return True
+
+# 101 对称二叉树
+# 递归求解
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            return p.val == q.val and check(q.left, p.right) and check(p.left, q.right)
+        return check(root.left, root.right)
+
+        
+        
+
